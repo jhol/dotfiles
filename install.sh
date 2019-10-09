c9db4636b9519585a9e97eb89f4affb2ae0cece9
Joel Holdsworth <joel@airwebreathe.org.uk>
2019-10-09 14:24:56 +0100

    install.sh: Refined installation process

--- install.sh
+++ install.sh
@@ -6,29 +6,40 @@ set -u
 
 install_path="$(cd "$(dirname "$0")"; pwd -P)"
 
+sudo -v
+
 #
 # Install the tools
 #
 
-sudo apt-get install tmux vim zsh
+echo "Installing packages"
+sudo apt-get install -y -qq tmux vim zsh
 
 #
 # Install the configs
 #
 
+echo "Installing config symbolic links"
 cd $HOME
 (cd $install_path; git ls-files) | grep -v -e 'install.sh' -e 'README.md' | while read f; do
-  ln -fs $(realpath --relative-to=$HOME ${install_path}/$f) .$f; done
+  echo "  .$f"
+  ln -fs $(realpath --relative-to=$HOME ${install_path}/$f) .$f;
+done
 
 #
 # Configure vim
 #
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "Installing vim plugins"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim 2>/dev/null
 vim +'PlugInstall --sync' +qa
 
 #
 # Configure Zsh
 #
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if [ ! -d ${HOME}/.oh-my-zsh ]; then
+  echo "Installing oh-my-zsh"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh 2>/dev/null)"
+fi
