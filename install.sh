c58525c9bb2d0c03999715d98f85bc19a9d8e5a5
Joel Holdsworth <joel@airwebreathe.org.uk>
2020-02-20 20:28:01 +0000

    install.sh: Don't use neovim PPA

--- install.sh
+++ install.sh
@@ -13,8 +13,6 @@ sudo true
 #
 
 echo "Installing packages"
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
 sudo apt-get install -y -qq \
   tmux \
