e8d49a880ff38d22ad5713e126b6e30036f85c3f
Joel Holdsworth <joel@airwebreathe.org.uk>
2022-02-28 12:27:46 +0000

    init.lua: Converted Colour Scheme section to lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -76,38 +76,29 @@ vim.call('plug#end')
 -- Colour Scheme
 --
 
-vim.api.nvim_exec(
-[[
+vim.o.termguicolors = (not vim.env.TMUX and vim.fn.has('termguicolors') == 1)
 
-if (empty($TMUX))
-  if (has("termguicolors"))
-    set termguicolors
-  endif
-endif
+vim.api.nvim_command("colorscheme base16-default-dark")
 
-colorscheme base16-default-dark
+vim.call('Base16hi', 'Normal', vim.g.base16_gui05, '000000', vim.g.base16_cterm05, vim.g.base16_cterm00, '', '')
+vim.call('Base16hi', 'LineNr', vim.g.base16_gui03, '000000', vim.g.base16_cterm03, vim.g.base16_cterm00, '', '')
 
-call g:Base16hi("Normal", g:base16_gui05, "000000", g:base16_cterm05, g:base16_cterm00, "", "")
-call g:Base16hi("LineNr", g:base16_gui03, "000000", g:base16_cterm03, g:base16_cterm00, "", "")
-
-let g:terminal_color_0  = '#2e3436'
-let g:terminal_color_1  = '#cc0000'
-let g:terminal_color_2  = '#4e9a06'
-let g:terminal_color_3  = '#c4a000'
-let g:terminal_color_4  = '#3465a4'
-let g:terminal_color_5  = '#75507b'
-let g:terminal_color_6  = '#0b939b'
-let g:terminal_color_7  = '#d3d7cf'
-let g:terminal_color_8  = '#555753'
-let g:terminal_color_9  = '#ef2929'
-let g:terminal_color_10 = '#8ae234'
-let g:terminal_color_11 = '#fce94f'
-let g:terminal_color_12 = '#729fcf'
-let g:terminal_color_13 = '#ad7fa8'
-let g:terminal_color_14 = '#00f5e9'
-let g:terminal_color_15 = '#eeeeec'
-
-]], true)
+vim.g.terminal_color_0  = '#2e3436'
+vim.g.terminal_color_1  = '#cc0000'
+vim.g.terminal_color_2  = '#4e9a06'
+vim.g.terminal_color_3  = '#c4a000'
+vim.g.terminal_color_4  = '#3465a4'
+vim.g.terminal_color_5  = '#75507b'
+vim.g.terminal_color_6  = '#0b939b'
+vim.g.terminal_color_7  = '#d3d7cf'
+vim.g.terminal_color_8  = '#555753'
+vim.g.terminal_color_9  = '#ef2929'
+vim.g.terminal_color_10 = '#8ae234'
+vim.g.terminal_color_11 = '#fce94f'
+vim.g.terminal_color_12 = '#729fcf'
+vim.g.terminal_color_13 = '#ad7fa8'
+vim.g.terminal_color_14 = '#00f5e9'
+vim.g.terminal_color_15 = '#eeeeec'
 
 --
 -- Additional Synax Highlighting
