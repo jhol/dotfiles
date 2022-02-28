f988530eb3db5df58591ba887fbab0efbceba133
Joel Holdsworth <joel@airwebreathe.org.uk>
2022-02-28 12:31:49 +0000

    init.lua: Converted AirLine section to lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -110,16 +110,11 @@ vim.api.nvim_command("au BufRead,BufNewFile *.[sS] set filetype=gas")
 -- AirLine
 --
 
-vim.api.nvim_exec(
-[[
-
-let g:airline_powerline_fonts = 1
-let g:airline_theme = 'base16'
-let g:airline#extensions#tabline#enabled = 1
-let g:airline#extensions#tabline#fnamemod = ':t'
-let g:airline_section_error = airline#section#create_right(['%{g:asyncrun_status}'])
-
-]], true)
+vim.g.airline_powerline_fonts = 1
+vim.g.airline_theme = 'base16'
+vim.g['airline#extensions#tabline#enabled'] = 1
+vim.g['airline#extensions#tabline#fnamemod'] = ':t'
+vim.api.nvim_command("let g:airline_section_error = airline#section#create_right(['%{g:asyncrun_status}'])")
 
 --
 -- Key Binding
