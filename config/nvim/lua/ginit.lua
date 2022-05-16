8b8bf5b48b69429aca80380e6e0df2be4bec2b3a
Joel Holdsworth <joel@airwebreathe.org.uk>
2022-05-16 15:41:15 +0100

    ginit.lua: Added GuiFullScreenToggle

--- config/nvim/lua/ginit.lua
+++ config/nvim/lua/ginit.lua
@@ -13,3 +13,24 @@ vim.call('GuiFont', 'Source Code Pro for Powerline:h8')
 --
 
 vim.call('rpcnotify', 0, 'Gui', 'Option', 'Tabline', 0)
+
+--
+-- Full Screen Toggle
+--
+
+vim.api.nvim_exec(
+[[
+
+function! s:GuiFullScreenToggle()
+  if g:GuiWindowFullScreen == 0
+    call GuiWindowFullScreen(1)
+  else
+    call GuiWindowFullScreen(0)
+  endif
+endfunction
+
+command! GuiFullScreenToggle call s:GuiFullScreenToggle()
+
+]], true)
+
+vim.api.nvim_set_keymap('n', '<F11>', ':GuiFullScreenToggle<CR>', { noremap = false })
