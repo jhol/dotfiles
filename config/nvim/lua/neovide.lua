eb75788742ea237969ace3e79344634052f7484b
Joel Holdsworth <joel@airwebreathe.org.uk>
2023-01-09 15:59:34 +0000

    neovide.lua: Added headings

--- config/nvim/lua/neovide.lua
+++ config/nvim/lua/neovide.lua
@@ -1,3 +1,16 @@
+--
+-- Neovide Configuration
+--
+
+--
+-- Font
+--
+
 vim.o.guifont = 'Source Code Pro for Powerline:h8.0';
+
+--
+-- Animation
+--
+
 vim.g.neovide_refresh_rate = 60;
 vim.g.neovide_cursor_animation_length = 0.01;
