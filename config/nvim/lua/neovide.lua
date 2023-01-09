e48657116e7f933683538e730bdb4495eb42cea5
Joel Holdsworth <joel@airwebreathe.org.uk>
2023-01-09 16:03:25 +0000

    neovide: Implemented full-screen toggle

--- config/nvim/lua/neovide.lua
+++ config/nvim/lua/neovide.lua
@@ -14,3 +14,20 @@ vim.o.guifont = 'Source Code Pro for Powerline:h8.0';
 
 vim.g.neovide_refresh_rate = 60;
 vim.g.neovide_cursor_animation_length = 0.01;
+
+--
+-- Full Screen Toggle
+--
+
+vim.api.nvim_exec(
+[[
+
+function! s:GuiFullScreenToggle()
+  let g:neovide_fullscreen=!g:neovide_fullscreen
+endfunction
+
+command! GuiFullScreenToggle call s:GuiFullScreenToggle()
+
+]], true)
+
+vim.api.nvim_set_keymap('n', '<F11>', ':GuiFullScreenToggle<CR>', { noremap = false })
