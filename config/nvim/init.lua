6f3c335815849f95a1a83c01a7af28667e764d55
Joel Holdsworth <joel@airwebreathe.org.uk>
2023-05-12 15:26:36 +0100

    plugins.lua: Removed Nuake

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -314,16 +314,6 @@ for _, lsp in pairs(servers) do
 end
 
 --
--- Nuake
---
-
-vim.g.nuake_position = 'left'
-
-vim.api.nvim_set_keymap('n', '<F4>', '<cmd>Nuake<CR>', { noremap = true })
-vim.api.nvim_set_keymap('i', '<F4>', '<C-\\><C-n><cmd>Nuake<CR>', { noremap = true })
-vim.api.nvim_set_keymap('t', '<F4>', '<C-\\><C-n><cmd>Nuake<CR>', { noremap = true })
-
---
 -- zoomer.vim
 --
 
--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -10,7 +10,6 @@ require('packer').startup(function()
   use 'jlanzarotta/bufexplorer'
   use { 'junegunn/fzf', run = 'cd ~/.fzf; ./install --all' }
   use 'junegunn/fzf.vim'
-  use 'Lenovsky/nuake'
   use 'LnL7/vim-nix'
   use 'neovim/nvim-lspconfig'
   use {
