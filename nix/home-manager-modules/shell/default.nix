20980b3f8af72a14a3ec8737892ad90550169961
Joel Holdsworth <joel@airwebreathe.org.uk>
2023-06-28 13:22:28 +0100

    nix/shell: Added bash configuration

--- nix/home-manager-modules/shell/default.nix
+++ nix/home-manager-modules/shell/default.nix
@@ -7,44 +7,54 @@ in
     enable = lib.mkEnableOption "Enable shell configuration";
   };
 
-  config = lib.mkIf cfg.enable {
-    programs.zsh = {
-      enable = true;
-
-      history.size = 10000;
-
-      localVariables = {
-        ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE = "fg=13,underline";
-        ZSH_AUTOSUGGEST_STRATEGY = [ "history" "completion" ];
-        KEYTIMEOUT = 1;
+  config = lib.mkIf cfg.enable (
+    let
+      historySize = 10000;
+    in
+    {
+      programs.bash = {
+        inherit historySize;
+        enable = true;
       };
 
-      enableAutosuggestions = true;
-      enableCompletion = true;
-      enableSyntaxHighlighting = true;
-      enableVteIntegration = true;
-      historySubstringSearch.enable = true;
+      programs.zsh = {
+        enable = true;
 
-      plugins = [
-        {
-          name = "zsh-history-substring-search";
-          file = "zsh-history-substring-search.zsh";
-          src = "${pkgs.zsh-history-substring-search}/share/zsh-history-substring-search";
-        }
-        {
-          name = "nix-shell";
-          src = "${pkgs.zsh-nix-shell}/share/zsh-nix-shell";
-        }
-        {
-          name = "powerlevel10k";
-          src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k";
-        }
-      ];
+        history.size = historySize;
 
-      initExtra = ''
-        source "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme"
-        source "${./p10k.zsh}"
-      '';
-    };
-  };
+        localVariables = {
+          ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE = "fg=13,underline";
+          ZSH_AUTOSUGGEST_STRATEGY = [ "history" "completion" ];
+          KEYTIMEOUT = 1;
+        };
+
+        enableAutosuggestions = true;
+        enableCompletion = true;
+        enableSyntaxHighlighting = true;
+        enableVteIntegration = true;
+        historySubstringSearch.enable = true;
+
+        plugins = [
+          {
+            name = "zsh-history-substring-search";
+            file = "zsh-history-substring-search.zsh";
+            src = "${pkgs.zsh-history-substring-search}/share/zsh-history-substring-search";
+          }
+          {
+            name = "nix-shell";
+            src = "${pkgs.zsh-nix-shell}/share/zsh-nix-shell";
+          }
+          {
+            name = "powerlevel10k";
+            src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k";
+          }
+        ];
+
+        initExtra = ''
+          source "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme"
+          source "${./p10k.zsh}"
+        '';
+      };
+    }
+  );
 }
