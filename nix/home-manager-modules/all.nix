110d633f3d205011ad81cc27040c4501d18fc0d7
Joel Holdsworth <joel@airwebreathe.org.uk>
2023-06-29 13:37:00 +0100

    nix: Added all home-manager module

---
+++ nix/home-manager-modules/all.nix
@@ -0,0 +1,19 @@
+{ lib, pkgs, config, ... }:
+let
+  cfg = config.modules.jhol-dotfiles.all;
+in
+{
+  options.modules.jhol-dotfiles.all = {
+    enable = lib.mkEnableOption "Enable all home-manager modules";
+  };
+
+  config = lib.mkIf cfg.enable {
+    modules.jhol-dotfiles = {
+      git.enable = true;
+      neovim.enable = true;
+      shell.enable = true;
+      terminal.enable = true;
+      tools.enable = true;
+    };
+  };
+}
