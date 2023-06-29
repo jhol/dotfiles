47058b7c95b43ca23c69daf813d754babc4725f4
Joel Holdsworth <joel@airwebreathe.org.uk>
2023-06-29 13:29:31 +0100

    nix: Added tools home-manager module

---
+++ nix/home-manager-modules/tools.nix
@@ -0,0 +1,20 @@
+{ lib, pkgs, config, ... }:
+let
+  cfg = config.modules.jhol-dotfiles.tools;
+in
+{
+  options.modules.jhol-dotfiles.tools = {
+    enable = lib.mkEnableOption "Enable command-line tools installation";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home = {
+      sessionPath = [ "$HOME/.local/bin" ];
+      file.".local/bin" = {
+        source = ../../bin;
+        recursive = true;
+        executable = true;
+      };
+    };
+  };
+}
