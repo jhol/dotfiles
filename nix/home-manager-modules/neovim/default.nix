499bc52359d25d64c3d2e03cd9ae7c5e92f8ef5f
Joel Holdsworth <joel@airwebreathe.org.uk>
2023-07-03 15:12:49 +0100

    neovim: vim.lsp.buf.formatting has been renamed to vim.lsp.buf.format

--- nix/home-manager-modules/neovim/default.nix
+++ nix/home-manager-modules/neovim/default.nix
@@ -357,7 +357,7 @@ in
               vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
               vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
               vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.format()<CR>', opts)
               vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
               vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
               vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
@@ -379,7 +379,7 @@ in
                 ['<space>'] = {
                   D = { vim.lsp.buf.type_definition(), "LSP Type Definitions" },
                   ["ca"] = { vim.lsp.buf.code_action(), "LSP Code Action" },
-                  f = { vim.lsp.buf.formatting(), "LSP Formatting" },
+                  f = { vim.lsp.buf.format(), "LSP Formatting" },
                   ["rn"] = { vim.lsp.buf.rename(), "LSP Rename" },
                   w = {
                     a = { vim.lsp.buf.add_workspace_folder(), "LSP Add Workspace Folder" },
