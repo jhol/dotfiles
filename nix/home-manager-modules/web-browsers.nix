5f1372e9b544fb13b4a6d8bdbe84d79da0e54440
Joel Holdsworth <joel@airwebreathe.org.uk>
2023-11-23 15:04:04 +0000

    web-browsers: Removed extraChromiumExtensions

--- nix/home-manager-modules/web-browsers.nix
+++ nix/home-manager-modules/web-browsers.nix
@@ -5,73 +5,6 @@ in
 {
   options.modules.jhol-dotfiles.web-browsers = {
     enable = mkEnableOption "Enable Web Browsers";
-
-    extraChromiumExtensions = mkOption {
-      type = with types;
-        let
-          extensionType = submodule {
-            options = {
-              id = mkOption {
-                type = strMatching "[a-zA-Z]{32}";
-                description = ''
-                  The extension's ID from the Chrome Web Store url or the unpacked crx.
-                '';
-                default = "";
-              };
-
-              updateUrl = mkOption {
-                type = str;
-                description = ''
-                  URL of the extension's update manifest XML file. Linux only.
-                '';
-                default = "https://clients2.google.com/service/update2/crx";
-              };
-
-              crxPath = mkOption {
-                type = nullOr path;
-                description = ''
-                  Path to the extension's crx file. Linux only.
-                '';
-                default = null;
-              };
-
-              version = mkOption {
-                type = nullOr str;
-                description = ''
-                  The extension's version, required for local installation. Linux only.
-                '';
-                default = null;
-              };
-            };
-          };
-        in listOf (coercedTo str (v: { id = v; }) extensionType);
-      default = [ ];
-      example = literalExpression ''
-        [
-          { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # ublock origin
-          {
-            id = "dcpihecpambacapedldabdbpakmachpb";
-            updateUrl = "https://raw.githubusercontent.com/iamadamdev/bypass-paywalls-chrome/master/updates.xml";
-          }
-          {
-            id = "aaaaaaaaaabbbbbbbbbbcccccccccc";
-            crxPath = "/home/share/extension.crx";
-            version = "1.0";
-          }
-        ]
-      '';
-      description = ''
-        List of ${name} extensions to install.
-        To find the extension ID, check its URL on the
-        [Chrome Web Store](https://chrome.google.com/webstore/category/extensions).
-
-        To install extensions outside of the Chrome Web Store set
-        `updateUrl` or `crxPath` and
-        `version` as explained in the
-        [Chrome
-        documentation](https://developer.chrome.com/docs/extensions/mv2/external_extensions).
-      '';
-    };
   };
 
   config = mkIf cfg.enable (let
@@ -87,7 +20,7 @@ in
         { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; }  # uBlock Origin
         { id = "oboonakemofpalcgghocfoadofidjkkk"; }  # KeePassXC-Browser
         { id = "pkehgijcmpdhfbdbbnkijodmdjhbjlgp"; }  # Privacy Badger
-      ] ++ cfg.extraChromiumExtensions;
+      ];
     };
 
     programs.firefox = {
