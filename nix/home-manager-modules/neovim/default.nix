ea9d9d4a303bdae287e940155b508c120f0ea577
Joel Holdsworth <joel@airwebreathe.org.uk>
2024-12-02 11:48:27 -0800

    neovim: Replaced <Cmd> with <cmd>

--- nix/home-manager-modules/neovim/default.nix
+++ nix/home-manager-modules/neovim/default.nix
@@ -130,10 +130,10 @@ in
             require("which-key").register({
               ['<leader>f'] = {
                 name = '+telescope',
-                a = { '<Cmd>Telescope live_grep<CR>', 'Telescope Grep Search' },
-                b = { '<Cmd>Telescope buffers<CR>', 'Telescope Buffer Search' },
-                f = { '<Cmd>Telescope find_files<CR>', 'Telescope File Search' },
-                q = { '<Cmd>Telescope command_history<CR>', 'Telescope Command History Search' },
+                a = { '<cmd>Telescope live_grep<CR>', 'Telescope Grep Search' },
+                b = { '<cmd>Telescope buffers<CR>', 'Telescope Buffer Search' },
+                f = { '<cmd>Telescope find_files<CR>', 'Telescope File Search' },
+                q = { '<cmd>Telescope command_history<CR>', 'Telescope Command History Search' },
               }
             });
           '';
@@ -155,8 +155,8 @@ in
             require("which-key").register({
               ['<leader>m'] = {
                 name = '+make',
-                m = { '<Cmd>AsyncRun -cwd=<root> -program=make<CR>', 'Make' },
-                c = { '<Cmd>AsyncStop<CR>', 'Cancel Make' }
+                m = { '<cmd>AsyncRun -cwd=<root> -program=make<CR>', 'Make' },
+                c = { '<cmd>AsyncStop<CR>', 'Cancel Make' }
               }
             });
           '';
@@ -169,8 +169,8 @@ in
             require("which-key").register({
               ['<leader>g'] = {
                 name = '+git',
-                v = { '<Cmd>Flogsplit<CR>', 'Git Visualize Branch' },
-                V = { '<Cmd>Flogsplit -all<CR>', 'Git Visualize All Branches' }
+                v = { '<cmd>Flogsplit<CR>', 'Git Visualize Branch' },
+                V = { '<cmd>Flogsplit -all<CR>', 'Git Visualize All Branches' }
               }
             });
           '';
@@ -187,10 +187,10 @@ in
             require("which-key").register({
               ['<leader>g'] = {
                 name = '+git',
-                a = { '<Cmd>Gwrite<CR>', 'Git Add File' },
-                c = { '<Cmd>Gcommit<CR>', 'Git Commit' },
-                p = { '<Cmd>Git add -p<CR>', 'Git Add Patch' },
-                s = { '<Cmd>Git<CR>', 'Git Status' },
+                a = { '<cmd>Gwrite<CR>', 'Git Add File' },
+                c = { '<cmd>Gcommit<CR>', 'Git Commit' },
+                p = { '<cmd>Git add -p<CR>', 'Git Add Patch' },
+                s = { '<cmd>Git<CR>', 'Git Status' },
               }
             });
           '';
@@ -346,29 +346,29 @@ in
             --
 
             local opts = { noremap=true, silent=true }
-            vim.api.nvim_set_keymap('n', '<space>e', '<Cmd>lua vim.diagnostic.open_float()<CR>', opts)
-            vim.api.nvim_set_keymap('n', '[d', '<Cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-            vim.api.nvim_set_keymap('n', ']d', '<Cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-            vim.api.nvim_set_keymap('n', '<space>q', '<Cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+            vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+            vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+            vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+            vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
 
             lsp_on_attach = function(client, bufnr)
               -- Enable completion triggered by <c-x><c-o>
               vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
               -- Mappings.
-              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<Cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<Cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<Cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<Cmd>lua vim.lsp.buf.format()<CR>', opts)
-              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<Cmd>lua vim.lsp.buf.rename()<CR>', opts)
-              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<Cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<Cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<Cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-              vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-              vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-              vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-              vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<Cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-              vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
+              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.format()<CR>', opts)
+              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+              vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+              vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+              vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+              vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+              vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
 
               -- Use LSP as the handler for omnifunc
               vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
@@ -454,7 +454,7 @@ in
             vim.cmd('command! Ex NERDTreeToggleInCurDir')
 
             require("which-key").register({
-              ['<leader>n'] = { '<Cmd>NERDTreeToggleInCurDir<CR>', 'Toggle NERDTree' },
+              ['<leader>n'] = { '<cmd>NERDTreeToggleInCurDir<CR>', 'Toggle NERDTree' },
             })
           '';
         }
@@ -533,7 +533,7 @@ in
 
         -- Update which-key
         require("which-key").register({
-          ['<F11>'] = { '<Cmd>GuiFullScreenToggle<CR>', 'Toggle Full Screen' }
+          ['<F11>'] = { '<cmd>GuiFullScreenToggle<CR>', 'Toggle Full Screen' }
         })
 
         --
