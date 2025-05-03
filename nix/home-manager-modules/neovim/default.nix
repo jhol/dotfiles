56a2368e28d4d0f452416b206892cc1941996ab6
Joel Holdsworth <joel@airwebreathe.org.uk>
2025-05-03 16:50:43 -0700

    neovim: Added Git rebase --continue shortcut

--- nix/home-manager-modules/neovim/default.nix
+++ nix/home-manager-modules/neovim/default.nix
@@ -183,6 +183,7 @@ in
               { '<leader>ga', '<cmd>Gwrite<cr>', desc = 'Git Add File' },
               { '<leader>gc', '<cmd>Gcommit<cr>', desc = 'Git Commit' },
               { '<leader>gp', '<cmd>Git add -p<cr>', desc = 'Git Add Patch' },
+              { '<leader>gr', '<cmd>Git rebase --continue<cr>', desc = 'Git Rebase Continue' },
               { '<leader>gs', '<cmd>Git<cr>', desc = 'Git Status' }
             });
           '';
