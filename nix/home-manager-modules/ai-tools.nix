55c49d21c520ec7cd209525af9427d92746fa3f4
Joel Holdsworth <joel@airwebreathe.org.uk>
2026-04-22 16:24:00 -0700

    ai-tools: Add LSPs

--- nix/home-manager-modules/ai-tools.nix
+++ nix/home-manager-modules/ai-tools.nix
@@ -7,6 +7,34 @@
 let
   cfg = config.modules.jhol-dotfiles.ai-tools;
 
+  lspServers = {
+    clangd = {
+      package = pkgs.clang-tools;
+      command = [ "clangd" ];
+    };
+    nixd = {
+      package = pkgs.nixd;
+      command = [ "nixd" ];
+    };
+    pyright = {
+      package = pkgs.pyright;
+      command = [
+        "pyright-langserver"
+        "--stdio"
+      ];
+    };
+    rust = {
+      package = pkgs.rust-analyzer;
+      command = [ "rust-analyzer" ];
+    };
+  };
+
+  lspPackages = lib.mapAttrsToList (_: s: s.package) lspServers;
+
+  lspSettings = lib.mapAttrs (_: s: {
+    command = [ "${s.package}/bin/${builtins.head s.command}" ] ++ builtins.tail s.command;
+  }) lspServers;
+
   skills =
     let
       caveman-src = pkgs.fetchFromGitHub {
@@ -31,10 +59,28 @@ in
 {
   options.modules.jhol-dotfiles.ai-tools = {
     enable = lib.mkEnableOption "Enable AI Tools";
+
+    opencodePackage = lib.mkPackageOption pkgs "opencode" { };
   };
 
   config = lib.mkIf cfg.enable {
-    programs.opencode.enable = true;
+    programs.opencode = {
+      enable = true;
+
+      package = pkgs.symlinkJoin {
+        inherit (cfg.opencodePackage) meta;
+        name = "${lib.getName cfg.opencodePackage}-with-lsp";
+        paths = [ cfg.opencodePackage ];
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+        postBuild = ''
+          wrapProgram $out/bin/opencode \
+            --suffix PATH : ${lib.makeBinPath lspPackages} \
+            --set OPENCODE_DISABLE_LSP_DOWNLOAD true
+        '';
+      };
+
+      settings.lsp = lspSettings;
+    };
 
     # TODO: Install using programs.opencode.skills after 26.05 release
     xdg.configFile = lib.mapAttrs' (
